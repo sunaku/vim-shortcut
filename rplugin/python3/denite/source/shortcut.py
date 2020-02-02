5478f7e43ee9333c4ed19e658824d6cfb4792b00
thecontinium <thecontinium@outlook.com>
2020-02-02 14:10:35 +0000

    move to correct denite dir

---
+++ rplugin/python3/denite/source/shortcut.py
@@ -0,0 +1,41 @@
+# ============================================================================
+# FILE: shortcut.py
+# ============================================================================
+
+from os import path
+
+from denite.base.source import Base
+from denite.kind.command import Kind as Command
+
+from denite.util import globruntime, Nvim, UserContext, Candidates
+
+
+class Source(Base):
+
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+
+        self.name = 'shortcut'
+        self.kind = Kind(vim)
+
+
+
+    def gather_candidates(self, context: UserContext) -> Candidates:
+        shortcuts= {}
+
+        for shortcut, description in self.vim.vars["shortcuts"].items():
+            shortcuts [shortcut] = {
+                'word': shortcut + '->' + description,
+                'action__command': shortcut
+            }
+
+        return sorted(shortcuts.values(), key=lambda value: value['word'])
+
+
+class Kind(Command):
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+        self.name = 'shortcut'
+
+    def action_edit(self, context: UserContext) -> None:
+        return super().action_execute(context)
